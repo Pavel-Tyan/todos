bba59cc31f2dfc648ae37534063cfd8ac0c7b123
Tyan_Pavel <ptyan_1@edu.hse.ru>
2024-10-10 18:26:16 +0300

    feat: add components in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
-import "./App.module.css";
+import styles from "./App.module.css";
+import { Input } from "./components/Input/Input";
+import { TodoList } from "./components/TodoList/TodoList";
+import { TodoProvider } from "./components/TodoProvider/TodoProvider";
 
 function App() {
-  return <></>;
+  return (
+    <div className={styles.wrapper}>
+      <TodoProvider>
+        <Input />
+        <TodoList />
+      </TodoProvider>
+    </div>
+  );
 }
 
 export default App;
